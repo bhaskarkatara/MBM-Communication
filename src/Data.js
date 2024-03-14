762a2d5853ef9e33ad0a21b06c330f60e83ca172
bhaskar katara <bhaskarkatararjo5@gmail.com>
2024-03-14 23:08:10 +0530

    add-images view functionality

--- src/Data.js
+++ src/Data.js
@@ -1,72 +1,76 @@
+import imag1 from '../assests/aman.png';
+import imag2 from '../assests/bhaskar.jpg';
+import imag3 from '../assests/munmun.jpeg';
+import imag4 from '../assests/rakhi.png';
+import imag5 from '../assests/aman.png';
 const data = [
   {
     uid: 1,
     name: 'Bhaskar',
-    imageurl: 'https://via.placeholder.com/150', // todo add images path
-
+    imageurl: imag2, // todo add images path
     message: 'hlo i am bhaskar katara',
   },
 
   {
     uid: 2,
     name: 'Rakhi katara',
-    imageurl: 'https://via.placeholder.com/150',
+    imageurl: imag1,
     message: 'hloo ji i am muskaan katara',
   },
 
   {
     uid: 3,
     name: 'aman katara',
-    imageurl: 'https://via.placeholder.com/150',
+    imageurl: imag3,
     message: 'hloo ji i am aman katara',
   },
 
   {
     uid: 4,
     name: 'munmun katara',
-    imageurl: 'https://via.placeholder.com/150',
+    imageurl: imag4,
     message: 'hloo ji i am munmun katara',
   },
   {
     uid: 5,
     name: 'Sachin ',
-    imageurl: 'https://via.placeholder.com/150',
+    imageurl: imag5,
     message: 'mai chutiya hu',
   },
   {
     uid: 6,
     name: 'namana katara',
-    imageurl: 'https://via.placeholder.com/150',
+    imageurl: imag1,
     message: 'cool boy katara',
   },
   {
     uid: 7,
     name: 'vandu',
-    imageurl: 'https://via.placeholder.com/150',
+    imageurl: imag4,
     message: ' kar dia na paraya',
   },
   {
     uid: 8,
     name: 'mohit jindal',
-    imageurl: 'https://via.placeholder.com/150',
+    imageurl: imag2,
     message: 'hloo boss ',
   },
   {
     uid: 9,
     name: 'raghuraj katara',
-    imageurl: 'https://via.placeholder.com/150',
+    imageurl: imag3,
     message: 'hloo ji i am raghuraj katara',
   },
   {
     uid: 10,
     name: ' lala katara',
-    imageurl: 'https://via.placeholder.com/150',
+    imageurl: imag1,
     message: 'hloo ji i am lala katara',
   },
   {
     uid: 11,
     name: 'pappu katara',
-    imageurl: 'https://via.placeholder.com/150',
+    imageurl: imag5,
     message: 'hloo ji i am pappu katara',
   },
 ];
--- src/GroupsScreen.js
+++ src/GroupsScreen.js
@@ -8,37 +8,74 @@ import {
   StyleSheet,
   Touchable,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import data from '../src/Data.js';
 
 const Groups = ({navigation}) => {
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isShow, setIsShow] = useState(null);
 
   const onClick = () => {
     setIsSubmitted(value => !value);
   };
-
+  // todo : check why after clicking one time ..not able to goes on chatScreen
+  // clicking double ,able to navigate on chat screen
   useEffect(() => {
     if (isSubmitted === true) {
       navigation.navigate('chatScreen');
     }
   }, [isSubmitted]);
+  const onShowUserImage = uid => {
+    for (let i = 0; i < data.length; i++) {
+      if (uid == data[i].uid) {
+        setIsShow(data[i].imageurl);
+        break;
+      }
+    }
+  };
 
+  const renderLargeImage = () => {
+    if (isShow) {
+      return (
+        <View style={style.modalContainer}>
+          <Image source={isShow} style={style.largeImage} />
+        </View>
+      );
+    }
+    return null;
+  };
+
+  const onCancelUserImage = () => {
+    setIsShow(null);
+  };
   return (
     <SafeAreaView style={{flex: 1}}>
-      <ScrollView style={style.container} scrollEnabled={true}>
+      <ScrollView
+        onPress={() => onCancelUserImage}
+        style={style.container}
+        scrollEnabled={true}>
         {data.map(item => (
           <View key={item.uid} style={style.userCard}>
-            <Image source={{uri: item.imageurl}} style={style.userImage} />
+            <TouchableOpacity onPress={() => onShowUserImage(item.uid)}>
+              <Image source={item.imageurl} style={style.userImage} />
+            </TouchableOpacity>
             <TouchableOpacity onPress={onClick}>
-              <View>
-                <Text style={style.userName}>{item.name}</Text>
-                <Text style={style.userMessage}>{item.message}</Text>
+              <View
+                style={{
+                  width: 305,
+                  height: 55,
+                }}>
+                <View>
+                  <Text style={style.userName}>{item.name}</Text>
+                  <Text style={style.userMessage}>{item.message}</Text>
+                </View>
               </View>
             </TouchableOpacity>
           </View>
         ))}
+        {renderLargeImage()}
       </ScrollView>
     </SafeAreaView>
   );
@@ -56,6 +93,20 @@ const style = StyleSheet.create({
     padding: 4,
     borderRadius: 14,
     // gap: 1,
+  },
+  modalContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  largeImage: {
+    width: 300,
+    height: 300,
+    borderRadius: 10,
   },
   container: {
     paddingHorizontal: 16,
